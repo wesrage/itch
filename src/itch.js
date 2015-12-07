6ac508e0fa8634ddc3a21d45496a148f9580eb3d
Wesley Waldner <wes@gignation.ca>
2015-12-07 00:29:03 -0600

    Added base set of features Supports matching by equality by default or by custom matching function Added built-in array-item matching support

---
+++ src/itch.js
@@ -0,0 +1,31 @@
+const strictEquals = (seed, candidate) => seed === candidate;
+const arrayMatcher = (matcher) =>
+   (seed, candidates) => candidates.some(c => matcher(seed, c));
+
+const done = (result) => {
+   const match = () => ({
+      then: () => done(result),
+   });
+   return {
+      match,
+      matchOneOf: match,
+      scratch: () => result,
+      using: () => done(result),
+   };
+};
+
+const notDone = (seed, matcher = strictEquals) => {
+   const match = (candidate, newMatcher = matcher) => ({
+      then: (newMatcher(seed, candidate))
+         ? (result) => done(result)
+         : () => notDone(seed, matcher),
+   });
+   return {
+      match,
+      matchOneOf: candidates => match(candidates, arrayMatcher(matcher)),
+      scratch: a => a,
+      using: newMatcher => notDone(seed, newMatcher),
+   };
+};
+
+export default (value) => notDone(value);
